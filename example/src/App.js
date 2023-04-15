7e2f3ea13dbefac1c2c34f253b1d5bc9af3d21be
Mustafa Güler <mustfguler@hotmail.com>
2023-04-15 12:09:35 +0300

    Button is created.

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-import { ExampleComponent } from 'wocbutton'
+import { Button } from 'wocbutton'
 import 'wocbutton/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      {/* button tipleri test edildi */}
+      <Button type='primary' text='Primary Button' />
+      <Button type='dashed' text='Error Button' />
+      <Button type='text' text='Warning Button' />
+      <Button type='link' text='Link Button' />
+      <Button text='Default Button' />
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Button = (props) => {
+  // Button selection with entered prop type
+  if (props.type === 'primary') {
+    return <button className={styles.success}>{props.text}</button>
+  } else if (props.type === 'dashed') {
+    return <button className={styles.error}>{props.text}</button>
+  } else if (props.type === 'text') {
+    return <button className={styles.warning}>{props.text}</button>
+  } else if (props.type === 'link') {
+    return <button className={styles.link}>{props.text}</button>
+  } else return <button className={styles.default}>{props.text}</button>
 }
